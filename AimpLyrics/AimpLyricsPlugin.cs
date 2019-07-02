8f839b38ba038edd0cf6448176785612ef2863c8
Andrey Arekhva <a.arekhva@gmail.com>
2019-07-02 13:56:10 +0300

    Add AIMP.SDK

---
+++ AimpLyrics/AimpLyricsPlugin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using AIMP.SDK;
+
+namespace AimpLyrics
+{
+    [AimpPlugin("AimpLyrics", "Andrey Arekhva", "0.0.1", AimpPluginType = AimpPluginType.Addons, Description = "Lyrics Plugin")]
+    public class AimpLyricsPlugin : AimpPlugin
+    {
+        public override void Initialize()
+        {
+            Console.WriteLine("Initialize");
+        }
+
+        public override void Dispose()
+        {
+            Console.WriteLine("Dispose");
+        }
+    }
+}
