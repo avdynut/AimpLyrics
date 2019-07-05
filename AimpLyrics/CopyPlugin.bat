3c763b8ca87402f23d6cf94f869a1cdb4714a720
Andrey Arekhva <a.arekhva@gmail.com>
2019-07-05 16:07:19 +0300

    Add batch file for copying plugin files to AIMP folder

---
+++ AimpLyrics/CopyPlugin.bat
@@ -0,0 +1,4 @@
+set pluginFolder=D:\Programs\AIMP\Plugins\AimpLyrics
+copy aimp_dotnet.dll %pluginFolder%\AimpLyrics.dll
+copy AimpLyrics.dll %pluginFolder%\AimpLyricsPlugin.dll
+copy AIMP.SDK.dll %pluginFolder%
