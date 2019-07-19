ea5a0f15b486693df41de1a8a2ece031cb7bd796
Andrey Arekhva <a.arekhva@gmail.com>
2019-07-19 13:14:35 +0300

    Display source of found lyrics #7

--- AimpLyrics/LyricsWIndow.xaml.cs
+++ AimpLyrics/LyricsWIndow.xaml.cs
@@ -22,31 +22,51 @@ namespace AimpLyrics
             _player = player;
             _player.ServiceLyrics.LyricsReceive += OnLyricsReceived;
 
-            hook.TrackChanged += () => GetCurrentLyrics();
-            GetCurrentLyrics();
+            hook.TrackChanged += () =>
+            {
+                ClearLyrics();
+                UpdateSongInfo();
+            };
+
+            UpdateSongInfo();
         }
 
-        private void GetCurrentLyrics()
+        private void UpdateSongInfo()
         {
+            Artist.Text = _player.CurrentFileInfo.Artist;
+            Title.Text = _player.CurrentFileInfo.Title;
             _player.ServiceLyrics.Get(_player.CurrentFileInfo, 0, null, out _);
+        }
+
+        private void ClearLyrics()
+        {
+            Lyrics.Text = "";
+            LyricsSource.Text = "";
         }
 
         private void OnLyricsReceived(IAimpLyrics lyrics, object userData)
         {
-            var fileInfo = _player.CurrentFileInfo;
-            Artist.Text = fileInfo.Artist;
-            Title.Text = fileInfo.Title;
-            Lyrics.Text = lyrics.Text.Length > 0 ? lyrics.Text : fileInfo.Lyrics;
-            Trace.WriteLine($"Lyrics received with text length: {lyrics.Text.Length}, from tag: {fileInfo.Lyrics.Length}");
+            if (!string.IsNullOrEmpty(lyrics.Text?.Trim()))
+            {
+                Lyrics.Text = lyrics.Text;
+                LyricsSource.Text = "AIMP Lyrics Service";
+            }
+            else if (!string.IsNullOrEmpty(_player.CurrentFileInfo.Lyrics?.Trim()))
+            {
+                Lyrics.Text = _player.CurrentFileInfo.Lyrics;
+                LyricsSource.Text = "Lyrics Tag";
+            }
+            else
+                SearchLyricsInGoogle();
 
-            if (string.IsNullOrEmpty(Lyrics.Text))
-                SearchLyricsInGoogle(this, null);
+            Trace.WriteLine($"Lyrics received with text length: {Lyrics.Text.Length}, from source: {LyricsSource.Text}");
         }
 
-        private void SearchLyricsInGoogle(object sender, RoutedEventArgs e)
+        private void SearchLyricsInGoogle()
         {
             string url = $"https://www.google.com/search?q={Artist.Text}+{Title.Text}";
             Browser.Navigate(url);
+            Trace.WriteLine($"Seaching lyrics by URL: {url}");
         }
 
         private void OnBrowserLoadCompleted(object sender, NavigationEventArgs e)
@@ -54,15 +74,24 @@ namespace AimpLyrics
             var doc = (HTMLDocument)Browser.Document;
             var nodes = doc.getElementsByTagName("g-expandable-content");
 
+            bool found = false;
             foreach (var node in nodes)
             {
                 var element = (IHTMLElement)node;
                 if (element.getAttribute("data-lyricid") is string && element.getAttribute("aria-hidden") == "true")
                 {
                     Lyrics.Text = element.innerText;
+                    found = true;
                     break;
                 }
             }
+            LyricsSource.Text = found ? "Google" : "None";
+        }
+
+        private void OnSearchButtonClick(object sender, RoutedEventArgs e)
+        {
+            ClearLyrics();
+            SearchLyricsInGoogle();
         }
 
         private void OnClosing(object sender, CancelEventArgs e)
