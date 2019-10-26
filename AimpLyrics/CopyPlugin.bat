3a9388f14bb5382cdf30869a831d99020f28d084
Andrey Arekhva <a.arekhva@gmail.com>
2019-10-26 10:53:34 +0300

    Use symbol link for debugging

--- AimpLyrics/CopyPlugin.bat
+++
@@ -1,6 +0,0 @@
-set aimpFolder=C:\Users\aarek\Desktop\AIMP_4.60.2146_debug\
-set pluginFolder=%aimpFolder%Plugins\AimpLyrics
-mkdir %pluginFolder%
-copy aimp_dotnet.dll %pluginFolder%\AimpLyrics.dll
-copy AimpLyrics.dll %pluginFolder%\AimpLyricsPlugin.dll
-copy AIMP.SDK.dll %pluginFolder%
